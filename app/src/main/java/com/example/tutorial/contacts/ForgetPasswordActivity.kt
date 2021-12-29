77d0d86c1c23b8aed63057e51138326534ec6e3d
gkzscs <gkzscs@hotmail.com>
2021-12-29 16:20:20 +0800

    Solve bugs, complete the application.

--- app/src/main/java/com/example/tutorial/contacts/ForgetPasswordActivity.kt
+++ app/src/main/java/com/example/tutorial/contacts/ForgetPasswordActivity.kt
@@ -2,6 +2,7 @@ package com.example.tutorial.contacts
 
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tutorial.R
 
@@ -16,6 +17,16 @@ class ForgetPasswordActivity : AppCompatActivity() {
     }
 
     fun searchPassword(view: View) {
+        val etAccount = findViewById<EditText>(R.id.et_account)
+        val account = etAccount.text.toString()
+        val listUsers = FileHelper.getUsers()
 
+        for (usr in listUsers) {
+            if (usr == null || usr.name != account) continue
+            FileHelper.showMessage(this, "您的密码为：${usr.password}")
+            return
+        }
+
+        FileHelper.showMessage(this, "未查询到该用户！")
     }
 }
--- app/src/main/java/com/example/tutorial/contacts/LoginActivity.kt
+++ app/src/main/java/com/example/tutorial/contacts/LoginActivity.kt
@@ -1,16 +1,40 @@
 package com.example.tutorial.contacts
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.CheckBox
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.tutorial.R
+import java.io.File
+import java.util.jar.Manifest
 
-class LoginActivity : AppCompatActivity(){
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts_login)
 
+        Log.i("LoginActivity", "创建")
+        handlePermission()
+
+        Log.i("LoginActivity", filesDir.absolutePath)
+        FileHelper.setFilePath(filesDir.absolutePath)
+
+        val etAccouont = findViewById<EditText>(R.id.et_account)
+        etAccouont.onFocusChangeListener = View.OnFocusChangeListener() { _: View, b: Boolean ->
+            if (b) return@OnFocusChangeListener
+
+            // Get last user and compare information
+            val lastUsr = FileHelper.lastUser() ?: return@OnFocusChangeListener
+            if (lastUsr.name != etAccouont.text.toString()) return@OnFocusChangeListener
+
+            val etPassword = findViewById<EditText>(R.id.et_password)
+            etPassword.setText(lastUsr.password)
+        }
     }
 
     fun register(view: View) {
@@ -18,11 +42,11 @@ class LoginActivity : AppCompatActivity(){
     }
 
     fun login(view: View) {
-        jumpPage(LoginActivity::class.java)
+        validateAccount()
     }
 
     fun rememberPassword(view: View) {
-
+        // Do nothing
     }
 
     fun forgetPassword(view: View) {
@@ -34,5 +58,83 @@ class LoginActivity : AppCompatActivity(){
         val intent = Intent()
         intent.setClass(this, cls)
         startActivity(intent)
+    }
+
+    private fun handlePermission() {
+        val permissions = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        for (permission in permissions) {
+            val selfPermission = ActivityCompat.checkSelfPermission(this, permission)
+            if (selfPermission == PackageManager.PERMISSION_GRANTED) {
+                Log.i("LoginActivity", "Already get permission")
+                continue
+            }
+        }
+
+        myRequestPermission()
+    }
+
+    private fun myRequestPermission() {
+        val permissions = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        Log.i("LoginActivity", "permissions:${permissions.first()}")
+        ActivityCompat.requestPermissions(this, permissions, 1)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        Log.i("LoginActivity", "grant results:${grantResults.size}, ${permissions.first()}")
+        var i = 0
+        for (result in grantResults) {
+            if (result == PackageManager.PERMISSION_GRANTED) {
+                Log.i("LoginActivity", "Got permission: ${permissions[i++]}")
+            } else {
+                Log.i("LoginActivity", "Denied permission: ${permissions[i++]}")
+            }
+        }
+    }
+
+    /**
+     * 验证用户信息
+     */
+    private fun validateAccount() : Boolean {
+        val etAccount = findViewById<EditText>(R.id.et_account)
+        val etPassword = findViewById<EditText>(R.id.et_password)
+        val account = etAccount.text.toString()
+        val password = etPassword.text.toString()
+        val listUsers = FileHelper.getUsers()
+
+        for (usr in listUsers) {
+            if (usr == null) continue
+            if (usr.name == account && usr.password == password) {
+                FileHelper.showMessage(this, "${usr.name}，登录成功！")
+                updateLastUser(account, password)
+                return true
+            }
+        }
+
+        FileHelper.showMessage(this, "用户名或密码错误！")
+        return false
+    }
+
+    /**
+     * 更新当前登录的用户信息
+     */
+    private fun updateLastUser(name: String, pwd: String) {
+        val chkRememberPwd = findViewById<CheckBox>(R.id.chk_remember_password)
+        val flag = chkRememberPwd.isChecked
+        var usr = User()
+
+        if (flag) {
+            usr.name = name
+            usr.password = pwd
+        }
+
+        FileHelper.setLastUser(usr)
     }
 }
